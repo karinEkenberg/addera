3ebcd8627b183fb4b057d0da02c884434c96e18b
karinEkenberg <137770952+karinEkenberg@users.noreply.github.com>
2023-11-29 17:14:51 +0100

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Var god skriv in en siffra:");
+            string dinSiffra = Console.ReadLine();
+            int nr1 = Convert.ToInt32(dinSiffra);
+            Console.WriteLine("Var god skriv ett nr till:");
+            string dinAndraSiffra = Console.ReadLine();
+            int nr2 = Convert.ToInt32(dinAndraSiffra);
+            int sum = nr1 + nr2;
+            Console.WriteLine("Dina siffror blir tillsammans " + sum + ".");
+        }
+    }
+}
